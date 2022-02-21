9a99e9e3807a2ad417b649eab7cb4b0acf27bdf0
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-02-21 19:27:35 +0000

    teste

---
+++ Idade.java
@@ -0,0 +1,9 @@
+public class Idade { 
+    public static void main(String[] args) {
+        int idade = 19;
+        System.out.println("Isabelly tem " +idade+" anos");
+        
+        
+    }
+    
+}
