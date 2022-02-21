23f0ecdeedf527ffda059dcbe67f740cac71f9f5
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-02-21 19:56:30 +0000

    teste3

--- Idade.java
+++ Idade.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Idade { 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num;
+        System.out.println("Digite um valor inteiro : ");
+        num = entrada.nextInt();
         int idade = 19;
         System.out.println("Isabelly tem " +idade+" anos");
         double media = 7.55;
